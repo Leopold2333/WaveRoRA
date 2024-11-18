9c6a8972d308ed49c8a74dc103e063b30856c307
Leopold2333 <3356076719@qq.com>
2024-11-18 10:28:43 +0800

    scripts-v1

--- scripts/LTSF/WaveRoRA/ETT.sh
+++ scripts/LTSF/WaveRoRA/ETT.sh
@@ -1,7 +1,7 @@
 project_path=$(pwd)
 work_space="$project_path/main.py"
 
-model="WaveFormer"
+model="WaveRoRA"
 seq_len=96
 loss="mse"
 random_seed=2025
@@ -11,13 +11,7 @@ w_dim=64
 d_model=256
 d_ff=256
 batch_size=32
-
-# dataset
-dataset_name="ETTh1"
-root_path=data/ETT-small
-file_name="${dataset_name}.csv"
 enc_in=7
-
 gpu=0
 
 lr="1e-4"
@@ -25,25 +19,34 @@ dropout=0.2
 
 is_training=$1
 
-for e_layers in 1 2
+# dataset
+dataset_name="ETTh1"
+file_name="${dataset_name}.csv"
+dataset_type=$dataset_name
+for pred_len in 96 192 336 720
 do
-    for dataset_name in ETTh1 ETTh2
-    do
-        file_name="${dataset_name}.csv"
-        dataset_type=$dataset_name
-        for pred_len in 96 192 336 720
-        do
-            log_file="logs/LTSF/${model}/${random_seed}(${dataset_name})_${model}(${seq_len}-${pred_len})_el${e_layers}.log"
-            python $work_space $model --is_training=$is_training --gpu=$gpu \
-            --num_workers=4 --seed=$random_seed --batch_size=$batch_size --loss=$loss \
-            --seq_len=$seq_len --pred_len=$pred_len --enc_in=$enc_in --use_norm --embed_type=0 \
-            --dataset_name=$dataset_name --file_name=$file_name --root_path=$root_path --dataset_type=$dataset_type \
-            --e_layers=$e_layers --wavelet_layers=$wavelet_layers --wavelet_type=sym3 --wavelet_mode=zero \
-            --d_model=$d_model --d_ff=$d_ff --wavelet_dim=$w_dim \
-            --learning_rate=$lr --dropout=$dropout \
-            > $log_file 2>&1
-        done
-    done
+    if [ "$pred_len" -eq 96 ]; then
+        lr="4e-4"
+        dropout=0.3
+    elif [ "$pred_len" -eq 192 ]; then
+        lr="1e-3"
+        dropout=0.1
+    elif [ "$pred_len" -eq 336 ]; then
+        lr="4e-4"
+        dropout=0.1
+    elif [ "$pred_len" -eq 720 ]; then
+        lr="4e-4"
+        dropout=0.1
+    fi
+    log_file="logs/LTSF/${model}/${random_seed}(${dataset_name})_${model}(${seq_len}-${pred_len})_el${e_layers}.log"
+    python $work_space $model --is_training=$is_training --gpu=$gpu \
+    --num_workers=4 --seed=$random_seed --batch_size=$batch_size --loss=$loss \
+    --seq_len=$seq_len --pred_len=$pred_len --enc_in=$enc_in --use_norm --embed_type=0 \
+    --dataset_name=$dataset_name --file_name=$file_name --root_path=$root_path --dataset_type=$dataset_type \
+    --e_layers=$e_layers --wavelet_layers=$wavelet_layers --wavelet_type=sym3 --wavelet_mode=zero \
+    --d_model=$d_model --d_ff=$d_ff --wavelet_dim=$w_dim \
+    --learning_rate=$lr --dropout=$dropout \
+    > $log_file 2>&1
 done
 
 
--- scripts/LTSF/WaveRoRA/electricity.sh
+++ scripts/LTSF/WaveRoRA/electricity.sh
@@ -24,28 +24,28 @@ gpu=0
 
 is_training=$1
 
-for pred_len in 96
+for pred_len in 96 192 336 720
 do
     if [ "$pred_len" -eq 96 ]; then
-        lr="1e-3"
-        dropout=0.1
+        lr="4e-4"
+        dropout=0.3
     elif [ "$pred_len" -eq 192 ]; then
         lr="1e-3"
-        dropout=0.2
+        dropout=0.1
     elif [ "$pred_len" -eq 336 ]; then
-        lr="5e-4"
+        lr="4e-4"
         dropout=0.1
     elif [ "$pred_len" -eq 720 ]; then
-        lr="5e-4"
+        lr="4e-4"
         dropout=0.1
     fi
     log_file="logs/LTSF/${model}/${random_seed}(${dataset_name})_${model}(${seq_len}-${pred_len})_el${e_layers}.log"
     python $work_space $model --is_training=$is_training --gpu=$gpu \
-    --num_workers=0 --seed=$random_seed --batch_size=$batch_size --loss=$loss \
+    --num_workers=4 --seed=$random_seed --batch_size=$batch_size --loss=$loss \
     --seq_len=$seq_len --pred_len=$pred_len --enc_in=$enc_in --use_norm --embed_type=0 --rotary --gate --residual \
     --dataset_name=$dataset_name --file_name=$file_name --root_path=$root_path --dataset_type=$dataset_type \
     --e_layers=$e_layers --wavelet_layers=$wavelet_layers --wavelet_type=sym3 --wavelet_mode=zero \
     --d_model=$d_model --d_ff=$d_ff --wavelet_dim=$w_dim \
     --learning_rate=$lr --dropout=$dropout \
-    # > $log_file 2>&1
+    > $log_file 2>&1
 done
